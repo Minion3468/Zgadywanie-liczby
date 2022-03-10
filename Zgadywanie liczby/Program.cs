a4ae434ab45e6533958fc996bfb5639af5ff13f4
Dominik <Dominik@JOJO-KOMP>
2022-03-10 20:57:06 +0100

    Dodaj pliki projektów.

---
+++ Zgadywanie liczby/Program.cs
@@ -0,0 +1,33 @@
+﻿static void Main(string[] args)
+{
+    Random mojaliczba = new Random();
+    int min = 1;
+    int max = 10;
+    int losowa;
+    int liczba;
+
+    while ()
+    {
+
+        losowa = 0;
+        liczba = mojaliczba.Next(min, max + 1);
+
+        while (losowa != liczba)
+        {
+            Console.WriteLine("podaj liczbę pomiędzy 1- 10: ");
+            losowa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj:");
+            if (losowa > liczba)
+            {
+                Console.WriteLine(losowa + "jest większa , sprobuj ponownie");
+            }
+            else if (losowa < liczba)
+            {
+                Console.WriteLine(losowa + " jest mniejsza , sprobuj ponownie");
+            }
+
+        }
+        Console.WriteLine("liczba:" + liczba);
+        Console.WriteLine("Wygrałeś gratulacje!Insert coin to play again");
+    }
+}
